Inverse Jacobian and fixed MC asymmetry
can now calculate inverse Jacobian with singularity, and fixed other problems inc. MC asymmetry between legs

kinematics.py
@@ -18,13 +18,14 @@ THIGH_LENGTH = 0.2 # [m]
 RIGHT = 0
 FORWARD = 1
 UP = 2
-HORIZONTAL = (FORWARD, RIGHT)
+HORIZONTAL = (RIGHT, FORWARD)
 RIGHT_DIR = np.array([1,0,0])
 FORWARD_DIR = np.array([0,1,0])
 UP_DIR = np.array([0,0,1])
 
 LEFTSCALE = np.ones(3)
-LEFTSCALE[RIGHT] *= -1 #multiply by this to change right to left
+LEFTSCALE[RIGHT] *= -1 # multiply vectors by LEFTSCALE to change right to left
+LEFTSCALE_L = LEFTSCALE.reshape((3,1)) # LEFTSCALE but for matrices instead of vectors
 
 def forward_kinematics(theta, body_inclination, return_Jacobian=False):
     """given motor angles, find the expected position of the foot.
@@ -65,6 +66,24 @@ def fk_Jacobian(theta, body_inclination):
     phi2 = phi1 + theta[2]
     phi = np.array([theta[0], phi1, phi2])
     return _fk_Jac_physTrig(np.sin(phi), np.cos(phi))
+
+def ik_Jacobian(fk_Jac, angle):
+    """find the inverse of the fk Jacobian. Deals with singularity.
+    fk_Jac is the forward jacobian, angle is the associated leg angles"""
+    max_width = 0.2 # the width in radians of the singularity handling
+    width = np.abs(angle[2])
+    if width > max_width:
+        ik_Jac = np.linalg.inv(fk_Jac)
+        return ik_Jac
+    else: #if we're close to the singularity
+        backup = np.empty((3,3))
+        backup[0:2,:] = np.linalg.pinv(fk_Jac[:,0:2])
+        backup[2,:] = 2 * backup[1,:]
+        if width > 0.02:
+            ik_Jac = np.linalg.inv(fk_Jac)
+            return (width / max_width) * ik_Jac + (1 - width / max_width) * backup
+        else: # too close to singularity for any inverse
+            return backup
 
 def fk_Jacobian_dot(theta, omega):
     """given leg angles and angular velocity, find the time derivative of the Jacobian returned by fk_Jacobian.
@@ -125,6 +144,7 @@ def _fk_Jac_physTrig(sines, cosines):
 
 def test():
     import time
+    import matplotlib.pyplot as plt
     rng = np.random.default_rng()
     numTries = 5000
     # thetas is [theta0 theta1 theta2 alpha] for each try
@@ -132,6 +152,7 @@ def test():
     fk_results = np.empty((3,numTries))
     Jac_results = np.empty((3,3,numTries))
     fk_Jac_results = np.empty((3,3,numTries))
+    ik_Jac_results = np.empty((3,3,numTries))
     ik_thetas = thetas[:3,:] + rng.uniform(low=-0.1, high=0.1, size=(3,numTries))
     ik_results = np.empty((3,numTries))
     ik_fails = np.zeros((numTries), dtype = int)
@@ -139,6 +160,7 @@ def test():
     forward_kinematics(np.zeros(3), 0.0)
     fk_Jacobian(np.zeros(3), 0.0) #compile
     
+    # Run everything and test timings
     startTime = time.perf_counter()
     for i in range(numTries):
         fk_results[:,i] = forward_kinematics(thetas[:3,i], thetas[3,i])
@@ -157,8 +179,29 @@ def test():
     fk_Jac_time = time.perf_counter() - startTime
     print("forward_kinematics with Jacobian takes", round(fk_Jac_time * 1e6 / numTries, 3), "us")
     
+    startTime = time.perf_counter()
+    for i in range(numTries):
+        ik_Jac_results[:,:,i] = ik_Jacobian(fk_Jac_results[:,:,i], thetas[:3,i])
+    ik_Jac_time = time.perf_counter() - startTime
+    print("inverse Jacobian takes", round(ik_Jac_time * 1e6 / numTries, 3), "us")
+    
+    # Test same results for forward_kinematics and fk_Jacobian
     if not np.all(np.isclose(Jac_results, fk_Jac_results)):
         print("FAIL: forward_kinematics and fk_Jacobian yielded different values")
+        
+    # test ik_Jacobian and fk_Jacobian match
+    eig = np.empty((3, numTries), dtype=np.complex64)
+    for i in range(numTries):
+        evals, _ = np.linalg.eig(ik_Jac_results[:,:,i] @ fk_Jac_results[:,:,i])
+        eig[:,i] = evals
+    print("ik_Jacobian and fk_Jacobian comparison, should be 1.0:", np.max(eig), np.min(eig))
+    fig = plt.figure(1)
+    fig.clf()
+    plt.plot(thetas[2,:], eig[0,:], '.', thetas[2,:], eig[1,:], '.', thetas[2,:], eig[2,:], '.')
+    plt.grid(True)
+    plt.xlabel("Knee Angle [rad]")
+    plt.ylabel("Eigenvalues (should be 1.0)")
+    plt.title("ik_Jacobian and fk_Jacobian comparison")
     
     startTime = time.perf_counter()
     for i in range(numTries):
@@ -180,6 +223,7 @@ def test():
             print("\t", forward_kinematics(ik_results[:,i], thetas[3,i]), " ||| ", fk_results[:,i])
             failnum += 1
     print(failnum, "inverse_kinematics incorrect results")
+    
     
         
     

motionController.py
@@ -3,8 +3,6 @@
 Created March 2021
 TODO:
     tests
-    same angles through right and left leg don't give symmetric results
-    deal with Jacobian singularity
     I'm sure MotionController_data isn't very pythonic but I refuse to read a style guide
 
 @author: Niraj
@@ -21,9 +19,9 @@ class MotionController:
     """A motion control calculator.
     use MC.set_plan() to set a new plan
     use MC.calc_controller() to calculate motor commands from angle sensors.
-    MC.dt is the time between calc_controller or set_leg_angles() calls
+    MC.dt is the time between calc_controller or set_leg_angles calls
     The first time you receive angle data, or any other time you have angle data
-        but don't run the controller, use MC.set_leg_angles()"""
+        but don't run calc_controller, use MC.set_leg_angles() instead to keep timing."""
     def __init__(self, dt = 1/20):
         """ constructor.
         dt is the time between calc_controller calls"""
@@ -137,8 +135,7 @@ class MotionController:
             estimated difference between current and desired motor angular velocity
         """
         pos, fk_Jac = kin.forward_kinematics(angle, body_inclination, return_Jacobian=True)
-        #TODO: deal with singularity
-        ik_Jac = np.linalg.inv(fk_Jac)
+        ik_Jac = kin.ik_Jacobian(fk_Jac, angle)
         ik_Jac_dot = (ik_Jac - self._ik_Jac) / self.dt
         self._ik_Jac = ik_Jac
         
@@ -191,7 +188,7 @@ class MotionController_data:
     def __init__(self):
         self.right_leg_conn = 0.03*kin.RIGHT_DIR
         self.left_leg_conn = -0.03*kin.RIGHT_DIR
-        self.body_weight = .2 * 9.81 # [N]
+        self.body_weight = .5 * 9.81 # [N]
         self.right_default_pos = -0.30*kin.UP_DIR + 0.05*kin.RIGHT_DIR
         self.left_default_pos = -0.30*kin.UP_DIR - 0.05*kin.RIGHT_DIR
         self.kp_1 = 5.0 #proportional gain on motor 1
@@ -219,34 +216,61 @@ def test():
     MC = MotionController()
     kin.forward_kinematics(np.zeros(3), 0.0, return_Jacobian=True) #compile
     MC.set_leg_angles(np.zeros(3), np.zeros(3))
-    numTries = 5000
+    numTries = 1000
     angles = np.array([[np.pi/2], [np.pi/2], [-np.pi]]) * np.random.random((3,numTries))
     inclination = np.random.random((numTries)) * np.pi/2
     
     rightCommands = np.empty((3,numTries))
     leftCommands = np.empty((3,numTries))
     
+    angles[:,0] = np.array([ 0.3631415 ,  1.44248313, -0.07108321])
+    
     #test calc_controller speed
     startTime = process_time()
     for i in range(numTries):
-        rightCommands[:,i], leftCommands[:,i] = MC.calc_controller(angles[:,i], -kin.RIGHT_DIR * angles[:,i], inclination[i])
+        rightCommands[:,i], leftCommands[:,i] = MC.calc_controller(angles[:,i], kin.LEFTSCALE * angles[:,i], inclination[i])
     calc_time = process_time() - startTime
     print("calc_controller takes", round(calc_time * 1e6 / numTries), "us")
     
-    if not np.all(np.isclose(rightCommands, leftCommands)):
+    #test identical left and right results
+    if np.all(np.isclose(rightCommands, kin.LEFTSCALE_L * leftCommands)):
+        print("Right and left commands were symmetrical")
+    else:
         print("FAIL: Right and left commands were not symmetrical")
-    asymmetries = 0
-    for i in range(numTries):
-        if not np.all(np.isclose(rightCommands[:,i], leftCommands[:,i])):
-            # print(rightCommands[:,i], leftCommands[:,i])
-            asymmetries += 1
-    print(asymmetries, "out of", numTries)
     
     #test leg angle error using inverse_kinematics
     angles_ref = angles + (np.random.random((3,numTries)) - 0.5) * 0.1
     
+    # test calc theta0, calc theta1, calc theta2
+    # move using each result, check you got closer to goal
+    resultAngles = np.empty((3, numTries))
+    resultAngles[0,:] = rightCommands[0,:]
+    resultAngles[1:,:] = angles[1:,:] + 0.01 * rightCommands[1:,:]
+    t0fails = 0
+    t1fails = 0
+    t2fails = 0
+    for i in range(numTries):
+        t0angles = np.array([rightCommands[0,i], angles[1,i], angles[2,i]])
+        t1angles = np.array([angles[0,i], angles[1,i] + 0.01 * rightCommands[1,i], angles[2,i]])
+        t2angles = np.array([angles[0,i], angles[1,i], angles[2,i] + 0.01 * rightCommands[2,i]])
+        
+        startDist = np.sum((MC.params.right_default_pos - kin.forward_kinematics(angles[:,i], inclination[i]))**2)
+        t0Dist = np.sum((MC.params.right_default_pos - kin.forward_kinematics(t0angles, inclination[i]))**2)
+        t1Dist = np.sum((MC.params.right_default_pos - kin.forward_kinematics(t1angles, inclination[i]))**2)
+        t2Dist = np.sum((MC.params.right_default_pos - kin.forward_kinematics(t2angles, inclination[i]))**2)
+        if t0Dist > startDist:
+            t0fails += 1
+        if t1Dist > startDist:
+            t1fails += 1
+        if t2Dist > startDist:
+            t2fails += 1
+    if t0fails > 0:
+        print("FAIL: Angle command 0 failed to improve tracking error", t0fails, "/", numTries, "times")
+    if t1fails > 0:
+        print("FAIL: Angle command 1 failed to improve tracking error", t1fails, "/", numTries, "times")
+    if t2fails > 0:
+        print("FAIL: Angle command 2 failed to improve tracking error", t2fails, "/", numTries, "times")
     
-    #test calc theta0, calc theta1, calc theta2
     
     # test parameters saving
     params = MotionController_data()