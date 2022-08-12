1a787dacfda4e69d45bc691bc1022758ddc62f9d
NirajPatelRobots <niraj01@gmail.com>
2022-08-11 20:19:51 -0400

    can display state orientation on desktop
    python script on desktop gets state information from robot and visualizes orientation with arrows.

--- display/displayState.py
+++ display/displayState.py
@@ -1,85 +1,53 @@
-""" Display the state of simplewalker on desktop base computer. Receives from simplewalker over wifi.
-June 2022
+""" Display the state of simplewalker on desktop base computer
+July 2022
 TODO:
 """
-
-import socket
+import numpy as np
 from xml.etree import ElementTree
-import dataclasses
+import vpython as v
+import remoteState
 from typing import List
-import struct
 
 
-@dataclasses.dataclass
-class RobotState:
-    msgID: int = 0
-    errcode: int = 0
-    timestamp_us: int = 0
-    pos: List[float] = dataclasses.field(default_factory = lambda: [0., 0., 0.])
-    axis: List[float] = dataclasses.field(default_factory = lambda: [0., 0., 0.])
-    vel: List[float] = dataclasses.field(default_factory = lambda: [0., 0., 0.])
-    angvel: List[float] = dataclasses.field(default_factory = lambda: [0., 0., 0.])
-    right_leg_pos: List[float] = dataclasses.field(default_factory = lambda: [0., 0., 0.])
-    left_leg_pos: List[float] = dataclasses.field(default_factory = lambda: [0., 0., 0.])
-    right_leg_vel: List[float] = dataclasses.field(default_factory = lambda: [0., 0., 0.])
-    left_leg_vel: List[float] = dataclasses.field(default_factory = lambda: [0., 0., 0.])
-    elementnames = ["pos", "axis", "vel", "angvel",
-                    "right_leg_pos", "left_leg_pos", "right_leg_vel", "left_leg_vel"]
-    vector_length = 3 * len(elementnames)
-    num_bytes = 8 + 4 * vector_length
-    struct_format = "hhl" + "f" * vector_length
+class DisplayRobot:
+    def __init__(self, state: remoteState.RemoteRobotState):
+        self.state = state
+        self.x_axis = v.vector(1, 0, 0)
+        self.y_axis = v.vector(0, 1, 0)
+        self.z_axis = v.vector(0, 0, 1)
+        self.x = v.arrow(axis=self.x_axis)
+        self.y = v.arrow(axis=self.y_axis)
+        self.z = v.arrow(axis=self.z_axis)
 
-    def set_from_msg(self, msg: bytes):
-        if len(msg) < self.num_bytes:
-            print(len(msg), "Byte message not long enough for state decode", end='\r')
-            return False
-        unpacked_state = struct.unpack(self.struct_format, msg)
-        self.msgID = unpacked_state[0]
-        self.errcode = unpacked_state[1]
-        self.timestamp_us = unpacked_state[2]
-        index = 3
-        for elementname in self.elementnames:
-            element = [0] * 3
-            for j in range(3):
-                element[j] = unpacked_state[index]
-                index +=1
-            setattr(self, elementname, element)
-        return True
-    
-    def __repr__(self) -> str:
-        return "pos:" + str(self.pos) + " axis:" + str(self.axis) \
-                + " vel:" + str(self.vel) + " angvel:" + str(self.angvel)
-
-
-def try_connect(target_host: str, target_port:int) -> socket.socket:
-    print("Connecting on", target_host, "::", target_port)
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.settimeout(5)
-    while True:
-        try:
-            client.connect((target_host, target_port))
-        except TimeoutError:
-            print("\rConnection attempt timed out...", end="")
-        except ConnectionRefusedError:
-            print("\rConnection refused by host...", end="")
+    def rotate(self, body_vector: v.vector):
+        angle = np.linalg.norm(self.state.axis)
+        if angle < 0.01:
+            unit_axis = v.vector(1, 0, 0)
         else:
-            break
-    return client
+            unit_axis = v.vector(self.state.axis[0], self.state.axis[1], self.state.axis[2]) / angle
+        return v.cos(angle) * body_vector + v.sin(angle) * v.cross(unit_axis, body_vector) \
+               + (1 - v.cos(angle)) * v.dot(unit_axis, body_vector) * unit_axis
 
-        
+    def update(self):
+        # self.x.pos = v.vector(state.pos)
+        # self.y.pos = v.vector(state.pos)
+        # self.z.pos = v.vector(state.pos)
+        self.x.axis = v.vector(self.rotate(self.x_axis))
+        self.y.axis = v.vector(self.rotate(self.y_axis))
+        self.z.axis = v.vector(self.rotate(self.z_axis))
+
+
 def main():
     settingsTree = ElementTree.parse("settings/settings.xml")
     settings = settingsTree.getroot()
-    state = RobotState()
-
-    target_host = socket.gethostbyname("raspberrypi")
-    target_port = int(settings.find("General").find("state_send_port").text)
-    client = try_connect(target_host, target_port)
+    state = remoteState.RemoteRobotState()
+    receiver = remoteState.BaseReceiver()
+    receiver.connect_state(int(settings.find("General").find("state_send_port").text))
+    displayRobot = DisplayRobot(state)
 
     while True:
-        state_msg_bytes = client.recv(state.num_bytes)
-        if state.set_from_msg(state_msg_bytes):
-            print(state, end='\r')
+        if state.set_from_msg(receiver.get_state_msg()):
+            displayRobot.update()
 
 
 if __name__ == '__main__':
---
+++ display/remoteState.py
@@ -0,0 +1,98 @@
+""" Print the state of simplewalker on desktop base computer. Receives from simplewalker over wifi.
+June 2022
+TODO:
+"""
+
+import socket
+from xml.etree import ElementTree
+import dataclasses
+from typing import List
+import struct
+
+
+@dataclasses.dataclass
+class RemoteRobotState:
+    """ State of simplewalker received by the base station. """
+    msgID: int = 0
+    errcode: int = 0
+    timestamp_us: int = 0
+    pos: List[float] = dataclasses.field(default_factory=lambda: [0., 0., 0.])
+    axis: List[float] = dataclasses.field(default_factory=lambda: [0., 0., 0.])
+    vel: List[float] = dataclasses.field(default_factory=lambda: [0., 0., 0.])
+    angvel: List[float] = dataclasses.field(default_factory=lambda: [0., 0., 0.])
+    right_leg_pos: List[float] = dataclasses.field(default_factory=lambda: [0., 0., 0.])
+    left_leg_pos: List[float] = dataclasses.field(default_factory=lambda: [0., 0., 0.])
+    right_leg_vel: List[float] = dataclasses.field(default_factory=lambda: [0., 0., 0.])
+    left_leg_vel: List[float] = dataclasses.field(default_factory=lambda: [0., 0., 0.])
+    elementnames = ["pos", "axis", "vel", "angvel",
+                    "right_leg_pos", "left_leg_pos", "right_leg_vel", "left_leg_vel"]
+    vector_length = 3 * len(elementnames)
+    num_bytes = 8 + 4 * vector_length
+    struct_format = "hhl" + "f" * vector_length
+
+    def set_from_msg(self, msg: bytes):
+        if len(msg) < self.num_bytes:
+            print(len(msg), "Byte message not long enough for state decode", end='\r')
+            return False
+        unpacked_state = struct.unpack(self.struct_format, msg)
+        self.msgID = unpacked_state[0]
+        self.errcode = unpacked_state[1]
+        self.timestamp_us = unpacked_state[2]
+        index = 3
+        for elementname in self.elementnames:
+            element = [0] * 3
+            for j in range(3):
+                element[j] = unpacked_state[index]
+                index += 1
+            setattr(self, elementname, element)
+        return True
+
+    def __repr__(self) -> str:
+        return "pos:" + str(self.pos) + " axis:" + str(self.axis) \
+               + " vel:" + str(self.vel) + " angvel:" + str(self.angvel)
+
+
+class BaseReceiver:
+    """ Connects to simplewalker and receives information"""
+    def __init__(self):
+        self.target_host = socket.gethostbyname("raspberrypi")
+        self.state_client_socket = None
+
+    def connect_state(self, target_port: int):
+        self.state_client_socket = self._blocking_connect(target_port)
+
+    def get_state_msg(self) -> bytes:
+        return self.state_client_socket.recv(RemoteRobotState.num_bytes)
+
+    def _blocking_connect(self, target_port: int) -> socket.socket:
+        print("Connecting on", self.target_host, "::", target_port)
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.settimeout(5)
+        while True:
+            try:
+                client.connect((self.target_host, target_port))
+            except TimeoutError:
+                print("\rConnection attempt timed out...", end="")
+            except ConnectionRefusedError:
+                print("\rConnection refused by host...", end="")
+            else:
+                break
+        return client
+
+
+def main():
+    settingsTree = ElementTree.parse("settings/settings.xml")
+    settings = settingsTree.getroot()
+    state = RemoteRobotState()
+
+    state_port = int(settings.find("General").find("state_send_port").text)
+    receiver = BaseReceiver()
+    receiver.connect_state(state_port)
+
+    while True:
+        if state.set_from_msg(receiver.get_state_msg()):
+            print(state, end='\r')
+
+
+if __name__ == '__main__':
+    main()
