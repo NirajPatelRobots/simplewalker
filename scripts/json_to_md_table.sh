a31ee2f158e79ad40e17587e64cbd5f1b5266337
niraj <niraj01@gmail.com>
2026-06-26 19:55:00 -0400

    json_to_md_table.sh highlights values that are different from the top row

--- scripts/json_to_md_table.sh
+++ scripts/json_to_md_table.sh
@@ -37,9 +37,23 @@ fi
 FIXED_COLS=$(printf '%s\n' "${FIXED_ARR[@]}" | jq -R . | jq -s . 2>/dev/null || echo "[]")
 IGNORE_COLS=$(printf '%s\n' "${IGNORE_ARR[@]}" | jq -R . | jq -s . 2>/dev/null || echo "[]")
 
-# Run the dynamic jq transformation
 jq -r --argjson fixed "$FIXED_COLS" --argjson ignore "$IGNORE_COLS" --argjson precision "$PRECISION" '
-  .[] |
+  def sanitize:  # clean and use $precision significant figures for floats
+    if type == "number" then
+      if . == 0 then "0"
+      else
+        # $mult is the 10^n multiplier to shift numbers to $precision digits to be rounded to int
+        # Ex: precision=4; value=1.234 -> mult=1000, value=12 -> mult=100, value>1000 -> mult=1
+        ([((($precision - 1) - (log10 | floor)) | pow(10; .)), 1] | max) as $mult |
+        ((. * $mult | round) / $mult)
+      end
+    else
+      (. // "-")  # Replace null with "-"
+    end | tostring
+  ;
+  def highlight: "<u>**" + . + "**</u>" ;
+  
+  .[] |  # The input to the next section is the array of tests. The outputs are the rows.
   if length == 0 then "Empty dataset" else
     # Gather all unique keys present across all items
     ([.[] | keys] | flatten | unique) as $all_keys |
@@ -53,20 +67,14 @@ jq -r --argjson fixed "$FIXED_COLS" --argjson ignore "$IGNORE_COLS" --argjson pr
     # Generate Divider Row (---)
     ([range($cols | length) | "---"] | map("| " + .)),
     
-    # Generate Data Rows (with $precision significant figures for floats)
-    (.[] as $row | $cols | map("| " + (
-      $row[.] | 
-      if type == "number" then
-        if . == 0 then "0"
-        else
-          # $mult is the 10^n multiplier to shift numbers to $precision digits to be rounded to int
-          # Ex: precision=4; value=1.234 -> mult=1000, value=12 -> mult=100, value>1000 -> mult=1
-          ([((($precision - 1) - (log10 | floor)) | pow(10; .)), 1] | max) as $mult |
-          ((. * $mult | round) / $mult | tostring)
-        end
-      else
-        (. // "-") | tostring
-      end
+    # Save the rows, and save the first row so we can compare other rows to it
+    .[] as $rows | .[0] as $compare_row |
+    
+    # Generate Data Rows
+    ($cols | map("| " + (
+      ($compare_row[.] | sanitize) as $compare_val |
+      $rows[.] | sanitize |
+      if . == $compare_val then . else highlight end
     )))
 
   end | join(" ") + " |"
