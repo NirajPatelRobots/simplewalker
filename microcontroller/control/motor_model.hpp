a409ab2b241b41655739a2e591de243b17ef8446
niraj <niraj01@gmail.com>
2026-08-30 01:34:21 -0400

    Destroy and test destruction of ModelFcns

--- microcontroller/control/motor_model.hpp
+++ microcontroller/control/motor_model.hpp
@@ -1,7 +1,15 @@
 /* See README.md #"Motor Models"
  * TODO:
+ *      FcnChain enforce initial base_fcn is a candidate base function (doesn't need parent)
+ *      ModelFcns different data structure instead of subclass?
+ *          Template<size_t num_params, float fcn(const model_inputs_t &model_inputs)>?
+ *          std::pair<size_t, float fcn(const model_inputs_t &model_inputs)>?
  *      Programatically create function chains from a command API
- *          storage structure for modelFcns, index is ID
+ *      State API and Input API. (TODO: Combine?) (TODO: Also read model API?)
+        - `Create_Term(base_fcn_id, weight)` -> `term_number`
+        - `Add_Function(term_number, nonbase_fcn_id)`
+        - `Set_Parameter(term_number)` TODO: how set if not first?
+ *      storage structure for modelFcns, index is ID
  */
 #include <array>
 #include <vector>
@@ -15,16 +23,18 @@ protected:
     ModelFcn *parent;
 public:
     ModelFcn(ModelFcn *_parent) : parent(_parent) {}
+    virtual ~ModelFcn() {delete parent;}
     // set_parent and set_param return true if the parent/param was set
     bool set_parent(ModelFcn *new_parent);
     virtual bool set_param([[maybe_unused]] unsigned num, [[maybe_unused]] float value) {return false;}
     virtual float call(const model_inputs_t &model_inputs) const = 0;
-    bool has_parent() {return (bool)parent;}
+    ModelFcn *get_parent() const {return parent;}
 };
 
-// FcnChain owns the memory for all of its ModelFcns except Singletons, which own themselves.
+// FcnChain owns the memory for its first_fcn, functions own their parent memory
 struct FcnChain {
     FcnChain(ModelFcn *base_fcn, float _weight) : first_fcn(base_fcn), weight(_weight) {}
+    ~FcnChain() {delete first_fcn;}
     ModelFcn *first_fcn;
     float weight;
     bool add_function(ModelFcn *new_fcn);
@@ -44,18 +54,17 @@ public:
 namespace ModelFcns {
     // base functions
     class Vel : public ModelFcn {
-        Vel() : ModelFcn(nullptr) {}; // singleton
+        Vel() : ModelFcn(nullptr) {}
     public:
-        static ModelFcn *create() {static Vel instance; return &instance;}
+        static ModelFcn *create() {return new Vel;}
         inline float call(const model_inputs_t &model_inputs) const override {
             return model_inputs.velocity;
         }
     };
-    
     class One : public ModelFcn {
-        One() : ModelFcn(nullptr) {}; // singleton
+        One() : ModelFcn(nullptr) {}
     public:
-        static ModelFcn *create() {static One instance; return &instance;}
+        static ModelFcn *create() {return new One;}
         inline float call([[maybe_unused]] const model_inputs_t &_) const override {
             return 1;
         }
--- test/microcontroller_tests/unittest_motor_model.cpp
+++ test/microcontroller_tests/unittest_motor_model.cpp
@@ -3,20 +3,38 @@
 #include "../../microcontroller/control/motor_model.hpp"
 
 const float TOLERANCE = 1e-6;
-model_inputs_t inputs = {.velocity = 104.3};
+const model_inputs_t INPUT_EXAMPLE = {.velocity = 104.3};
 
 
+class MockModelFcn : public ModelFcn {
+    MockModelFcn() : ModelFcn(nullptr) {
+        num_instances++;
+    };
+public:
+    static ModelFcn *create() {return new MockModelFcn;}
+    virtual ~MockModelFcn() {
+        num_instances--;
+    }
+    inline float call(const model_inputs_t &model_inputs) const override {
+        return parent->call(model_inputs);
+    }
+    static int num_instances;
+};
+int MockModelFcn::num_instances = 0 ;
+
+
+// Tested: sizeof(ModelFcns::x) / sizeof(char) = 16 byte
 TEST(FcnsTest, VelModelFcn) {
     auto vel_fcn = ModelFcns::Vel::create();
 
-    EXPECT_NEAR(inputs.velocity, vel_fcn->call(inputs), TOLERANCE);
+    EXPECT_NEAR(INPUT_EXAMPLE.velocity, vel_fcn->call(INPUT_EXAMPLE), TOLERANCE);
     EXPECT_FALSE(vel_fcn->set_param(0, 0.f));
 }
 
 TEST(FcnsTest, OneModelFcn) {
     auto one_fcn = ModelFcns::One::create();
 
-    EXPECT_EQ(1.f, one_fcn->call(inputs));
+    EXPECT_EQ(1.f, one_fcn->call(INPUT_EXAMPLE));
     EXPECT_FALSE(one_fcn->set_param(0, 0.f));
 }
 
@@ -24,7 +42,7 @@ TEST(FcnsTest, SignModelFcn) {
     auto sign_fcn = ModelFcns::Sign::create();
     sign_fcn->set_parent(ModelFcns::Vel::create());
 
-    inputs.velocity = 100.f;
+    model_inputs_t inputs = {.velocity = 100.f};
     EXPECT_EQ( 1.f, sign_fcn->call(inputs));  // intentional float equality comparisons
     inputs.velocity = -100.f;
     EXPECT_EQ(-1.f, sign_fcn->call(inputs));
@@ -39,20 +57,20 @@ TEST(FcnChainTest, Weight) {
     FcnChain term = FcnChain(ModelFcns::Vel::create(), weight);
 
     EXPECT_EQ(weight, term.weight);
-    EXPECT_NEAR(weight * inputs.velocity, term.call(inputs), TOLERANCE);
+    EXPECT_NEAR(weight * INPUT_EXAMPLE.velocity, term.call(INPUT_EXAMPLE), TOLERANCE);
 }
 
 TEST(FcnChainTest, OnlyVel) {
     FcnChain term = FcnChain(ModelFcns::Vel::create(), 1.0);
 
-    EXPECT_NEAR(inputs.velocity, term.call(inputs), TOLERANCE);
+    EXPECT_NEAR(INPUT_EXAMPLE.velocity, term.call(INPUT_EXAMPLE), TOLERANCE);
 }
 
 TEST(FcnChainTest, SignVel) {
     auto term = FcnChain(ModelFcns::Vel::create(), 1.0);
     term.add_function(ModelFcns::Sign::create());
 
-    inputs.velocity = 100.f;
+    model_inputs_t inputs = {.velocity = 100.f};
     EXPECT_EQ( 1.f, term.call(inputs));  // intentional float equality comparisons
     inputs.velocity = -100.f;
     EXPECT_EQ(-1.f, term.call(inputs));
@@ -60,7 +78,55 @@ TEST(FcnChainTest, SignVel) {
     EXPECT_EQ( 0.f, term.call(inputs));
 }
 
-// Test that modelFcns get deleted when FcnChain gets deleted, but singletons dont
+
+TEST(FcnChainTest, ThreeDeep) {
+    auto term = new FcnChain(ModelFcns::Vel::create(), 1.0);
+    term->add_function(MockModelFcn::create());
+    term->add_function(MockModelFcn::create());
+
+    EXPECT_EQ(INPUT_EXAMPLE.velocity, term->call(INPUT_EXAMPLE));
+    EXPECT_EQ(MockModelFcn::num_instances, 2);
+    
+    delete term;
+    EXPECT_EQ(MockModelFcn::num_instances, 0);
+}
+
+
+TEST(FcnChainTest, TwoChainzSameBase) {
+    auto term1 = new FcnChain(ModelFcns::Vel::create(), 1.0);
+    term1->add_function(MockModelFcn::create());
+
+    auto term2 = new FcnChain(ModelFcns::Vel::create(), 1.0);
+    term2->add_function(MockModelFcn::create());
+
+    EXPECT_EQ(MockModelFcn::num_instances, 2);
+    EXPECT_EQ(term1->call(INPUT_EXAMPLE), INPUT_EXAMPLE.velocity);
+    EXPECT_EQ(term2->call(INPUT_EXAMPLE), INPUT_EXAMPLE.velocity);
+    delete term1;
+    EXPECT_EQ(term2->call(INPUT_EXAMPLE), INPUT_EXAMPLE.velocity);
+    EXPECT_EQ(MockModelFcn::num_instances, 1);
+    delete term2;
+    EXPECT_EQ(MockModelFcn::num_instances, 0);
+}
+
+
+TEST(FcnChainTest, TwoChainzDifferentBase) {
+    auto termOne = new FcnChain(ModelFcns::One::create(), 1.0);
+    termOne->add_function(MockModelFcn::create());
+    
+    auto termVel = new FcnChain(ModelFcns::Vel::create(), 1.0);
+    termVel->add_function(MockModelFcn::create());
+    
+    model_inputs_t inputs = {.velocity = -100.f};
+
+    EXPECT_EQ(MockModelFcn::num_instances, 2);
+    EXPECT_EQ(termOne->call(inputs), 1.0);
+    EXPECT_EQ(termVel->call(inputs), inputs.velocity);
+    delete termOne;
+    EXPECT_EQ(termVel->call(inputs), inputs.velocity);
+    delete termVel;
+    EXPECT_EQ(MockModelFcn::num_instances, 0);
+}
 
 // test FcnChain last function (first entered) must be a type that doesn't reference a parent?
 
