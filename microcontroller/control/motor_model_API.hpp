f72b6c405befef3b2616235cbcc3671a65e7a701
Niraj Patel <72592523+NirajPatelRobots@users.noreply.github.com>
2026-09-07 23:43:05 -0400

    Create motor model (#2)
    Create motor model with API and several starting model functions. Core code is built by both host and microcontroller projects, for compatible API and for unit tests on the host machine. Described in Readme.

---
+++ microcontroller/control/motor_model_API.hpp
@@ -0,0 +1,50 @@
+#include <stdint.h>
+
+class MotorModel;
+
+namespace MotorModelAPI {
+
+enum class COMMAND : uint8_t {
+    HELLO, // no effect, set if command creation fails
+    CREATE_TERM,
+    DELETE_TERM,
+    ADD_FCN,
+    SET_PARAM,
+    SET_WEIGHT
+};
+enum class GROUP : uint8_t {
+    STATE, INPUT
+};
+const uint8_t MAX_FCN_IDX{0xF};
+const uint8_t MAX_PARAM_IDX{0xF};
+
+struct Request {
+    GROUP   group;
+    COMMAND command;
+    uint8_t term_idx;
+    uint8_t fcn_field;
+    float value;
+};
+
+struct Response {
+    int8_t status;
+    uint8_t term_idx;
+    enum STATUS : int8_t {
+        SUCCESS = 1, HELLO = 2,
+        UNSET = 0, BAD_REQUEST = -1, BAD_TERM_IDX = -2, BAD_FCN_FIELD = -3, BAD_PARAM = -4,
+    };
+    inline bool successful() {return status > 0;}
+};
+
+// Request API functions (run on main computer)
+bool msg_create_term(GROUP group, const char *base_fcn_name, float weight, Request &msg_out);
+bool msg_delete_term(GROUP group, unsigned term_idx, Request &msg_out);
+bool msg_add_function(GROUP group, unsigned term_idx, const char *nonbase_fcn_name, Request &msg_out);
+bool msg_set_parameter(GROUP group, unsigned term_idx, unsigned fcn_idx, unsigned param_idx, float value, Request &msg_out);
+bool msg_set_weight(GROUP group, unsigned term_idx, float weight, Request &msg_out);
+const Request HELLO_REQUEST {};
+
+// Microcontroller API handler function
+void handle_request(const Request &msg, MotorModel &model, Response &response);
+
+}
---
+++ microcontroller/control/motor_model_API_user.cpp
@@ -0,0 +1,95 @@
+#include "model_fcns.hpp"
+#include "motor_model_API.hpp"
+#include <string>
+#include <unordered_map>
+
+
+namespace MotorModelAPI {
+// helpers
+std::unordered_map<std::string, ModelFcn *(*)(void)> BASE_FUNCTION_NAME_MAP {
+    {"vel", ModelFcns::Vel::create},
+    {"one", ModelFcns::One::create},
+};
+
+std::unordered_map<std::string, ModelFcn *(*)(void)> NONBASE_FUNCTION_NAME_MAP {
+    {"mult", ModelFcns::Mult::create},
+    {"sign", ModelFcns::Sign::create},
+};
+
+uint8_t get_base_fcn_id(const char *fcn_name) {
+    const auto &creator_fcn = BASE_FUNCTION_NAME_MAP[fcn_name];
+    for (size_t i = 0; i < NUM_BASE_FUNCTIONS; i++) {
+        if (BASE_FUNCTIONS[i] == creator_fcn) return i;
+    }
+    return UINT8_MAX;
+}
+
+uint8_t get_nonbase_fcn_id(const char *fcn_name) {
+    const auto &creator_fcn = NONBASE_FUNCTION_NAME_MAP[fcn_name];
+    for (size_t i = 0; i < NUM_NONBASE_FUNCTIONS; i++) {
+        if (NONBASE_FUNCTIONS[i] == creator_fcn) return i;
+    }
+    return UINT8_MAX;
+}
+
+
+// message creation functions
+bool msg_create_term(GROUP group, const char *base_fcn_name, float weight, Request &msg_out) {
+    if (group > GROUP::INPUT) {msg_out.command = COMMAND::HELLO; return false;}
+    msg_out.fcn_field = get_base_fcn_id(base_fcn_name);
+    if (msg_out.fcn_field > NUM_BASE_FUNCTIONS) {
+        msg_out.command = COMMAND::HELLO;
+        return false;
+    }
+    msg_out.group = group;
+    msg_out.command = COMMAND::CREATE_TERM;
+    msg_out.value = weight;
+    return true;
+}
+
+
+bool msg_delete_term(GROUP group, unsigned term_idx, Request &msg_out) {
+    if (group > GROUP::INPUT) {msg_out.command = COMMAND::HELLO; return false;}
+    msg_out.group = group;
+    msg_out.command = COMMAND::DELETE_TERM;
+    msg_out.term_idx = term_idx;
+    return true;
+}
+
+
+bool msg_add_function(GROUP group, unsigned term_idx, const char *nonbase_fcn_name, Request &msg_out) {
+    if (group > GROUP::INPUT) {msg_out.command = COMMAND::HELLO; return false;}
+    msg_out.fcn_field = get_nonbase_fcn_id(nonbase_fcn_name);
+    if (msg_out.fcn_field > NUM_NONBASE_FUNCTIONS) {
+        msg_out.command = COMMAND::HELLO;
+        return false;
+    }
+    msg_out.group = group;
+    msg_out.command = COMMAND::ADD_FCN;
+    msg_out.term_idx = term_idx;
+    return true;
+}
+
+
+bool msg_set_parameter(GROUP group, unsigned term_idx, unsigned fcn_idx, unsigned param_idx, float value, Request &msg_out) {
+    if (group > GROUP::INPUT) {msg_out.command = COMMAND::HELLO; return false;}
+    if (fcn_idx > MAX_FCN_IDX) {msg_out.command = COMMAND::HELLO; return false;}
+    if (param_idx > MAX_PARAM_IDX) {msg_out.command = COMMAND::HELLO; return false;}
+    msg_out.group = group;
+    msg_out.command = COMMAND::SET_PARAM;
+    msg_out.term_idx = term_idx;
+    msg_out.fcn_field = ((fcn_idx & 0xF) << 4) + (param_idx & 0xF);
+    msg_out.value = value;
+    return true;
+}
+
+
+bool msg_set_weight(GROUP group, unsigned term_idx, float weight, Request &msg_out) {
+    if (group > GROUP::INPUT) {msg_out.command = COMMAND::HELLO; return false;}
+    msg_out.group = group;
+    msg_out.command = COMMAND::SET_WEIGHT;
+    msg_out.term_idx = term_idx;
+    msg_out.value = weight;
+    return true;
+}
+}
